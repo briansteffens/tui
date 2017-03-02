8fe7219621098ee99db603aaf17f859344b7342a
Brian Steffens <briansteffens@gmail.com>
2017-03-02 13:01:57 -0500

    Switch to tabs

--- main.go
+++ main.go
@@ -1,224 +1,224 @@
 package main
 
 import (
-    "fmt"
-    "github.com/nsf/termbox-go"
-    "github.com/briansteffens/escapebox"
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"github.com/briansteffens/escapebox"
 )
 
 func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func setCell(x, y int, r rune) {
-    termbox.SetCell(x, y, r, termbox.ColorWhite, termbox.ColorBlack)
+	termbox.SetCell(x, y, r, termbox.ColorWhite, termbox.ColorBlack)
 }
 
 func termPrintf(x, y int, format string, args ...interface{}) {
-    s := fmt.Sprintf(format, args...)
-    for i, c := range s {
-        setCell(x + i, y, c)
-    }
+	s := fmt.Sprintf(format, args...)
+	for i, c := range s {
+		setCell(x + i, y, c)
+	}
 }
 
 func renderBorder(r rect) {
-    // Corners
-    setCell(r.left, r.top, '+')
-    setCell(r.right(), r.top, '+')
-    setCell(r.left, r.bottom(), '+')
-    setCell(r.right(), r.bottom(), '+')
+	// Corners
+	setCell(r.left, r.top, '+')
+	setCell(r.right(), r.top, '+')
+	setCell(r.left, r.bottom(), '+')
+	setCell(r.right(), r.bottom(), '+')
 
-    // Horizontal borders
-    for x := r.left + 1; x < r.right(); x++ {
-        setCell(x, r.top, '-')
-        setCell(x, r.bottom(), '-')
+	// Horizontal borders
+	for x := r.left + 1; x < r.right(); x++ {
+		setCell(x, r.top, '-')
+		setCell(x, r.bottom(), '-')
 	}
 
-    // Vertical borders
-    for y := r.top + 1; y < r.bottom(); y++ {
-        setCell(r.left, y, '|')
-        setCell(r.right(), y, '|')
-    }
+	// Vertical borders
+	for y := r.top + 1; y < r.bottom(); y++ {
+		setCell(r.left, y, '|')
+		setCell(r.right(), y, '|')
+	}
 }
 
 type rect struct {
-    left, top, width, height int
+	left, top, width, height int
 }
 
 func (r* rect) right() int {
-    return r.left + r.width - 1
+	return r.left + r.width - 1
 }
 
 func (r* rect) bottom() int {
-    return r.top + r.height - 1
+	return r.top + r.height - 1
 }
 
 type control interface {
-    render()
+	render()
 }
 
 type focusable interface {
-    control
-    setFocus()
-    unsetFocus()
-    handleEvent(escapebox.Event)
+	control
+	setFocus()
+	unsetFocus()
+	handleEvent(escapebox.Event)
 }
 
 // label ----------------------------------------------------------------------
 
 type label struct {
-    bounds rect
-    text   string
+	bounds rect
+	text   string
 }
 
 func (l* label) render() {
-    termPrintf(l.bounds.left, l.bounds.top, l.text)
+	termPrintf(l.bounds.left, l.bounds.top, l.text)
 }
 
 // textbox --------------------------------------------------------------------
 
 type textbox struct {
-    bounds rect
-    value  string
-    cursor int
-    scroll int
-    focus  bool
+	bounds rect
+	value  string
+	cursor int
+	scroll int
+	focus  bool
 }
 
 func renderableChar(k termbox.Key) bool {
-    return k != termbox.KeyEnter  &&
-           k != termbox.KeyPgup   &&
-           k != termbox.KeyPgdn   &&
-           k != termbox.KeyInsert
+	return k != termbox.KeyEnter  &&
+	       k != termbox.KeyPgup   &&
+	       k != termbox.KeyPgdn   &&
+	       k != termbox.KeyInsert
 }
 
 func (t* textbox) maxVisibleChars() int {
-    return t.bounds.width - 2
+	return t.bounds.width - 2
 }
 
 func (t* textbox) visibleChars() int {
-    return min(t.maxVisibleChars(), len(t.value) - t.scroll)
+	return min(t.maxVisibleChars(), len(t.value) - t.scroll)
 }
 
 func (t* textbox) lastVisible() int {
-    return t.scroll + t.visibleChars() - 1
+	return t.scroll + t.visibleChars() - 1
 }
 
 func (t* textbox) render() {
-    renderBorder(t.bounds)
-    termPrintf(t.bounds.left + 1, t.bounds.top + 1,
-               t.value[t.scroll:t.lastVisible() + 1])
+	renderBorder(t.bounds)
+	termPrintf(t.bounds.left + 1, t.bounds.top + 1,
+		   t.value[t.scroll:t.lastVisible() + 1])
 
-    if t.focus {
-        termbox.SetCursor(t.bounds.left + 1 + t.cursor - t.scroll,
-                          t.bounds.top + 1)
-    }
+	if t.focus {
+		termbox.SetCursor(t.bounds.left + 1 + t.cursor - t.scroll,
+				  t.bounds.top + 1)
+	}
 }
 
 func (t* textbox) setFocus() {
-    t.focus = true
+	t.focus = true
 }
 
 func (t* textbox) unsetFocus() {
-    t.focus = false
+	t.focus = false
 }
 
 func (t* textbox) handleEvent(ev escapebox.Event) {
-    pre := t.value[0:t.cursor]
-    post := t.value[t.cursor:len(t.value)]
+	pre := t.value[0:t.cursor]
+	post := t.value[t.cursor:len(t.value)]
 
-    switch ev.Type {
-    case termbox.EventKey:
-        char := fmt.Sprintf("%c", ev.Ch)
+	switch ev.Type {
+	case termbox.EventKey:
+		char := fmt.Sprintf("%c", ev.Ch)
 
-        switch ev.Key {
-        case termbox.KeyBackspace, termbox.KeyBackspace2:
-            if len(pre) > 0 {
-                t.value = pre[0:len(pre)-1] + post
-                t.cursor--
-            }
-        case termbox.KeyDelete:
-            if len(post) > 0 {
-                t.value = pre + post[1:len(post)]
-            }
-        case termbox.KeyArrowLeft:
-            t.cursor--
-        case termbox.KeyArrowRight:
-            t.cursor++
-        case termbox.KeyHome:
-            t.cursor = 0
-        case termbox.KeyEnd:
-            t.cursor = len(t.value)
-        default:
-            if renderableChar(ev.Key) {
-                t.value = pre + char + post
-                t.cursor++
-            }
-        }
-    }
+		switch ev.Key {
+		case termbox.KeyBackspace, termbox.KeyBackspace2:
+			if len(pre) > 0 {
+				t.value = pre[0:len(pre)-1] + post
+				t.cursor--
+			}
+		case termbox.KeyDelete:
+			if len(post) > 0 {
+				t.value = pre + post[1:len(post)]
+			}
+		case termbox.KeyArrowLeft:
+			t.cursor--
+		case termbox.KeyArrowRight:
+			t.cursor++
+		case termbox.KeyHome:
+			t.cursor = 0
+		case termbox.KeyEnd:
+			t.cursor = len(t.value)
+		default:
+			if renderableChar(ev.Key) {
+				t.value = pre + char + post
+				t.cursor++
+			}
+		}
+	}
 
-    if t.cursor < 0 {
-        t.cursor = 0
-    }
+	if t.cursor < 0 {
+		t.cursor = 0
+	}
 
-    if t.cursor > len(t.value) {
-        t.cursor = len(t.value)
-    }
+	if t.cursor > len(t.value) {
+		t.cursor = len(t.value)
+	}
 
-    if t.cursor < t.scroll {
-        t.scroll = t.cursor
-    }
+	if t.cursor < t.scroll {
+		t.scroll = t.cursor
+	}
 
-    if t.cursor >= t.scroll + t.maxVisibleChars() {
-        t.scroll = t.cursor - t.maxVisibleChars() + 1
-    }
+	if t.cursor >= t.scroll + t.maxVisibleChars() {
+		t.scroll = t.cursor - t.maxVisibleChars() + 1
+	}
 }
 
 // checkbox -------------------------------------------------------------------
 
 type checkbox struct {
-    bounds  rect
-    text    string
-    checked bool
-    focus   bool
+	bounds  rect
+	text    string
+	checked bool
+	focus   bool
 }
 
 func (c* checkbox) render() {
-    checkContent := " "
+	checkContent := " "
 
-    if c.checked {
-        checkContent = "X"
-    }
+	if c.checked {
+		checkContent = "X"
+	}
 
 	s := fmt.Sprintf("[%s] %s", checkContent, c.text)
 
-    count := min(len(s), c.bounds.width)
-    termPrintf(c.bounds.left, c.bounds.top, s[0:count])
+	count := min(len(s), c.bounds.width)
+	termPrintf(c.bounds.left, c.bounds.top, s[0:count])
 
-    if c.focus {
-        termbox.SetCursor(c.bounds.left + 1, c.bounds.top)
-    }
+	if c.focus {
+		termbox.SetCursor(c.bounds.left + 1, c.bounds.top)
+	}
 }
 
 func (c* checkbox) setFocus() {
-    c.focus = true
+	c.focus = true
 }
 
 func (c* checkbox) unsetFocus() {
-    c.focus = false
+	c.focus = false
 }
 
 func (c* checkbox) handleEvent(ev escapebox.Event) {
-    switch ev.Type {
-    case termbox.EventKey:
-        switch ev.Key {
-        case termbox.KeySpace:
-            c.checked = !c.checked
-        }
-    }
+	switch ev.Type {
+	case termbox.EventKey:
+		switch ev.Key {
+		case termbox.KeySpace:
+			c.checked = !c.checked
+		}
+	}
 }
 
 // button ---------------------------------------------------------------------
@@ -226,69 +226,69 @@ func (c* checkbox) handleEvent(ev escapebox.Event) {
 type ButtonClickEvent func(*button)
 
 type button struct {
-    bounds       rect
-    text         string
-    focus        bool
-    clickHandler ButtonClickEvent
+	bounds       rect
+	text         string
+	focus        bool
+	clickHandler ButtonClickEvent
 }
 
 func (b* button) render() {
-    renderBorder(b.bounds)
+	renderBorder(b.bounds)
 
-    count := min(len(b.text), b.bounds.width - 4)
-    termPrintf(b.bounds.left + 2, b.bounds.top + 1, b.text[0:count])
+	count := min(len(b.text), b.bounds.width - 4)
+	termPrintf(b.bounds.left + 2, b.bounds.top + 1, b.text[0:count])
 
-    if b.focus {
-        termbox.SetCursor(b.bounds.left + 1, b.bounds.top + 1)
-    }
+	if b.focus {
+		termbox.SetCursor(b.bounds.left + 1, b.bounds.top + 1)
+	}
 }
 
 func (b* button) setFocus() {
-    b.focus = true
+	b.focus = true
 }
 
 func (b* button) unsetFocus() {
-    b.focus = false
+	b.focus = false
 }
 
 func (b* button) handleEvent(ev escapebox.Event) {
-    switch ev.Type {
-    case termbox.EventKey:
-        switch ev.Key {
-        case termbox.KeyEnter, termbox.KeySpace:
-            if b.clickHandler != nil {
-                b.clickHandler(b)
-            }
-        }
-    }
+	switch ev.Type {
+	case termbox.EventKey:
+		switch ev.Key {
+		case termbox.KeyEnter, termbox.KeySpace:
+			if b.clickHandler != nil {
+				b.clickHandler(b)
+			}
+		}
+	}
 }
 
 // editor ---------------------------------------------------------------------
 
 type editbox struct {
-    bounds rect
-    value  string
-    cursor int
-    scroll int
-    focus  bool
+	bounds rect
+	value  string
+	cursor int
+	scroll int
+	focus  bool
 }
 
 func (e* editbox) render() {
-    renderBorder(e.bounds)
-    termPrintf(e.bounds.left + 1, e.bounds.top + 1, e.value)
+	renderBorder(e.bounds)
+	termPrintf(e.bounds.left + 1, e.bounds.top + 1, e.value)
 
-    if e.focus {
-        termbox.SetCursor(e.bounds.left + 1 + e.cursor - e.scroll,
-                          e.bounds.top + 1)
-    }
+	if e.focus {
+		termbox.SetCursor(e.bounds.left + 1 + e.cursor - e.scroll,
+				  e.bounds.top + 1)
+	}
 }
 
 func (e* editbox) setFocus() {
-    e.focus = true
+	e.focus = true
 }
 
 func (e* editbox) unsetFocus() {
-    e.focus = false
+	e.focus = false
 }
 
 func (e* editbox) handleEvent(ev escapebox.Event) {
@@ -298,189 +298,190 @@ func (e* editbox) handleEvent(ev escapebox.Event) {
 // container ------------------------------------------------------------------
 
 type container struct {
-    controls []control
-    focused  focusable
+	controls []control
+	focused  focusable
 }
 
 func (c* container) focus(f focusable) {
-    if c.focused != nil {
-        c.focused.unsetFocus()
-    }
+	if c.focused != nil {
+		c.focused.unsetFocus()
+	}
 
-    c.focused = f
-    c.focused.setFocus()
+	c.focused = f
+	c.focused.setFocus()
 }
 
 func (c* container) focusNext() {
-    currentIndex := 0
+	currentIndex := 0
 
-    // Find index of currently focused control
-    if c.focused != nil {
-        for index, ctrl := range c.controls {
-            if ctrl == c.focused {
-                currentIndex = index
-                break
-            }
-        }
-    }
+	// Find index of currently focused control
+	if c.focused != nil {
+		for index, ctrl := range c.controls {
+			if ctrl == c.focused {
+				currentIndex = index
+				break
+			}
+		}
+	}
 
-    // Scan list after focused control for another focusable control
-    for i := currentIndex + 1; i < len(c.controls); i++ {
-        f, ok := c.controls[i].(focusable)
-        if ok {
-            c.focus(f)
-            return
-        }
-    }
+	// Scan list after focused control for another focusable control
+	for i := currentIndex + 1; i < len(c.controls); i++ {
+		f, ok := c.controls[i].(focusable)
+		if ok {
+			c.focus(f)
+			return
+		}
+	}
 
-    // Scan list before focused control (loop around)
-    for i := 0; i <= currentIndex; i++ {
-        f, ok := c.controls[i].(focusable)
-        if ok {
-            c.focus(f)
-            return
-        }
-    }
+	// Scan list before focused control (loop around)
+	for i := 0; i <= currentIndex; i++ {
+		f, ok := c.controls[i].(focusable)
+		if ok {
+			c.focus(f)
+			return
+		}
+	}
 }
 
 func (c* container) focusPrevious() {
-    currentIndex := 0
+	currentIndex := 0
 
-    // Find index of currently focused control
-    if c.focused != nil {
-        for index, ctrl := range c.controls {
-            if ctrl == c.focused {
-                currentIndex = index
-                break
-            }
-        }
-    }
+	// Find index of currently focused control
+	if c.focused != nil {
+		for index, ctrl := range c.controls {
+			if ctrl == c.focused {
+				currentIndex = index
+				break
+			}
+		}
+	}
 
-    // Scan list before focused control for another focusable control
-    for i := currentIndex - 1; i >= 0; i-- {
-        f, ok := c.controls[i].(focusable)
-        if ok {
-            c.focus(f)
-            return
-        }
-    }
+	// Scan list before focused control for another focusable control
+	for i := currentIndex - 1; i >= 0; i-- {
+		f, ok := c.controls[i].(focusable)
+		if ok {
+			c.focus(f)
+			return
+		}
+	}
 
-    // Scan list after focused control (loop around)
-    for i := len(c.controls) - 1; i >= currentIndex; i-- {
-        f, ok := c.controls[i].(focusable)
-        if ok {
-            c.focus(f)
-            return
-        }
-    }
+	// Scan list after focused control (loop around)
+	for i := len(c.controls) - 1; i >= currentIndex; i-- {
+		f, ok := c.controls[i].(focusable)
+		if ok {
+			c.focus(f)
+			return
+		}
+	}
 }
 
 func refresh(c container) {
-    termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 
-    for _, v := range c.controls {
-        v.render()
-    }
+	for _, v := range c.controls {
+		v.render()
+	}
 
-    termbox.Flush()
-    termbox.Sync()
+	termbox.Flush()
+	termbox.Sync()
 }
 
 // Non-standard escape sequences
 const (
-    SeqShiftTab = 1
+	SeqShiftTab = 1
 )
 
 func buttonClickHandler(b *button) {
-    panic("clicked!")
+	panic("clicked!")
 }
 
 func main() {
-    err := termbox.Init()
-    if err != nil {
-        panic(err)
-    }
-    defer termbox.Close()
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
-    termbox.SetInputMode(termbox.InputEsc) // | termbox.InputMouse)
+	termbox.SetInputMode(termbox.InputEsc) // | termbox.InputMouse)
 
-    escapebox.Init()
-    defer escapebox.Close()
+	escapebox.Init()
+	defer escapebox.Close()
 
-    escapebox.Register(SeqShiftTab, 91, 90)
+	escapebox.Register(SeqShiftTab, 91, 90)
 
-    edit1 := editbox {
-        bounds: rect { left: 2, top: 6, width: 30, height: 10 },
-        value: "Hello!",
-        cursor: 0,
-        scroll: 0,
-    }
+	edit1 := editbox {
+		bounds: rect { left: 2, top: 6, width: 30, height: 10 },
+		value: "Hello!",
+		cursor: 0,
+		scroll: 0,
+	}
 
-    l := label {
-        bounds: rect { left: 2, top: 1, width: 20, height: 1 },
-        text: "Greetings:",
-    }
+	l := label {
+		bounds: rect { left: 2, top: 1, width: 20, height: 1 },
+		text: "Greetings:",
+	}
 
-    t := textbox {
-        bounds: rect { left: 2, top: 2, width: 5, height: 3 },
-        value: "12",
-        cursor: 2,
-        scroll: 0,
-    }
+	t := textbox {
+		bounds: rect { left: 2, top: 2, width: 5, height: 3 },
+		value: "12",
+		cursor: 2,
+		scroll: 0,
+	}
 
-    t2 := textbox {
-        bounds: rect { left: 10, top: 2, width: 15, height: 3},
-        value: "Greetings!",
-        cursor: 0,
-        scroll: 0,
-    }
+	t2 := textbox {
+		bounds: rect { left: 10, top: 2, width: 15, height: 3},
+		value: "Greetings!",
+		cursor: 0,
+		scroll: 0,
+	}
 
-    checkbox1 := checkbox {
-        bounds: rect { left: 27, top: 1, width: 30, height: 1},
-        text: "Enable the whateverthing",
-    }
+	checkbox1 := checkbox {
+		bounds: rect { left: 27, top: 1, width: 30, height: 1},
+		text: "Enable the whateverthing",
+	}
 
-    button1 := button {
-        bounds: rect { left: 27, top: 2, width: 10, height: 3},
-        text: "Continue!",
-        clickHandler: buttonClickHandler,
-    }
+	button1 := button {
+		bounds: rect { left: 27, top: 2, width: 10, height: 3},
+		text: "Continue!",
+		clickHandler: buttonClickHandler,
+	}
 
-    c := container {
-        controls: []control {&t, &edit1, &l, &t2, &checkbox1, &button1},
-    }
+	c := container {
+		controls: []control {&t, &edit1, &l, &t2, &checkbox1,
+				     &button1},
+	}
 
-    c.focusNext()
-    refresh(c)
+	c.focusNext()
+	refresh(c)
 
-    loop: for {
-        ev := escapebox.PollEvent()
+	loop: for {
+		ev := escapebox.PollEvent()
 
-        handled := false
+		handled := false
 
-        switch ev.Seq {
-        case escapebox.SeqNone:
-            switch ev.Type {
-            case termbox.EventKey:
-                switch ev.Key {
-                case termbox.KeyCtrlA:
-                    l.text = ""
-                case termbox.KeyCtrlC:
-                    break loop
-                case termbox.KeyTab:
-                    c.focusNext()
-                    handled = true
-                }
-            }
-        case SeqShiftTab:
-            c.focusPrevious()
-            handled = true
-        }
+		switch ev.Seq {
+		case escapebox.SeqNone:
+			switch ev.Type {
+			case termbox.EventKey:
+				switch ev.Key {
+				case termbox.KeyCtrlA:
+					l.text = ""
+				case termbox.KeyCtrlC:
+					break loop
+				case termbox.KeyTab:
+					c.focusNext()
+					handled = true
+				}
+			}
+		case SeqShiftTab:
+			c.focusPrevious()
+			handled = true
+		}
 
-        if !handled && c.focused != nil {
-            c.focused.handleEvent(ev)
-        }
+		if !handled && c.focused != nil {
+			c.focused.handleEvent(ev)
+		}
 
-        refresh(c)
-    }
+		refresh(c)
+	}
 }
